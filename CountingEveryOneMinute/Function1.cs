13894544e7b2a0c72bc1b87e2cbbb4e026f1d5d6
sachincodes <joshisachin059@gmail.com>
2023-08-09 15:07:44 +0530

    added function

---
+++ CountingEveryOneMinute/Function1.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Data.SqlClient;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace CountingEveryOneMinute
+{
+    public class Function1
+    {
+        public static string _connection = "Server=tcp:azuretestingdbsachin.database.windows.net,1433;Initial Catalog=countingdb;Persist Security Info=False;User ID=sachin;Password=s@chin@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        public int count = 0;
+        [FunctionName("Function1")]
+        public void Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, ILogger log)
+        {
+            CreateTable();
+            count++;
+            AddEmailEntryToTheDatabase();
+        }
+
+        private void CreateTable()
+        {
+            using SqlConnection connection = new(_connection);
+            string query = @"IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'Counting')
+                            BEGIN
+                                CREATE TABLE Counting
+                                (
+                                    Id bigint primary key identity(1,1),
+                                    Count int not null,
+                                )
+                            END";
+            SqlCommand command = new(query, connection);
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+        private void AddEmailEntryToTheDatabase()
+        {
+            using SqlConnection connection = new(_connection);
+            string query = @"INSERT INTO Counting(Count)
+                             VALUES(@Count)";
+            SqlCommand command = new(query, connection);
+            command.Parameters.AddWithValue("@Count", count);
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
