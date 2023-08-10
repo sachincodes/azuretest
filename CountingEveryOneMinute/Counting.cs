9a6d1ed8ed58ccb843996b0a6d2c16c48f0c0544
sachincodes <joshisachin059@gmail.com>
2023-08-10 17:07:13 +0530

    used nested function instead

--- CountingEveryOneMinute/Counting.cs
+++ CountingEveryOneMinute/Counting.cs
@@ -8,20 +8,68 @@ namespace CountingEveryOneMinute
 {
     public class Counting
     {
-        public static string _connection = "Server=tcp:azuretestingdbsachin.database.windows.net,1433;Initial Catalog=countingdb;Persist Security Info=False;User ID=sachin;Password=s@chin@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-        public int count = 0;
         [FunctionName("Counting")]
-        public void Run([TimerTrigger("0 */1 * * * *", RunOnStartup = true, UseMonitor = true)]TimerInfo myTimer, ILogger log)
+        public void Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, ILogger log)
         {
+            string _connection = "Server=tcp:azuretestingdbserver.database.windows.net,1433;Initial Catalog=countingdb;Persist Security Info=False;User ID=sachin;Password=s@chin@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            long count = GetCurrentCount();
+            log.LogInformation($"Counting function executed at: {DateTime.Now}");
             CreateTable();
             count++;
             AddEmailEntryToTheDatabase();
-        }
 
-        private void CreateTable()
-        {
-            using SqlConnection connection = new(_connection);
-            string query = @"IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'Counting')
+            long GetCurrentCount()
+            {
+                long count = 0;
+                using SqlConnection connection = new(_connection);
+                string query = @"SELECT max(Id) as Max from Counting";
+                SqlCommand command = new(query, connection);
+                try
+                {
+                    connection.Open();
+                    var reader = command.ExecuteReader();
+                    while(reader.Read())
+                    {
+                        count = reader["Max"] is DBNull ? 0 : (long) reader["Max"];
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+                return count;
+            }
+
+            void AddEmailEntryToTheDatabase()
+            {
+                using SqlConnection connection = new(_connection);
+                string query = @"INSERT INTO Counting(Count)
+                             VALUES(@Count)";
+                SqlCommand command = new(query, connection);
+                command.Parameters.AddWithValue("@Count", count);
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+
+            void CreateTable()
+            {
+                using SqlConnection connection = new(_connection);
+                string query = @"IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'Counting')
                             BEGIN
                                 CREATE TABLE Counting
                                 (
@@ -29,41 +77,22 @@ namespace CountingEveryOneMinute
                                     Count int not null,
                                 )
                             END";
-            SqlCommand command = new(query, connection);
-            try
-            {
-                connection.Open();
-                command.ExecuteNonQuery();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                connection.Close();
-            }
-        }
-        private void AddEmailEntryToTheDatabase()
-        {
-            using SqlConnection connection = new(_connection);
-            string query = @"INSERT INTO Counting(Count)
-                             VALUES(@Count)";
-            SqlCommand command = new(query, connection);
-            command.Parameters.AddWithValue("@Count", count);
-            try
-            {
-                connection.Open();
-                command.ExecuteNonQuery();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                connection.Close();
+                SqlCommand command = new(query, connection);
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
             }
         }
+        
     }
 }
